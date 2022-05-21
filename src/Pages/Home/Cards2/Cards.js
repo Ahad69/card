4aa5138ed7150b10039fe8bfe6e3073137986a8f
Ahad hossain <ahadxx99@gmail.com>
2022-05-21 03:00:40 -0500

    first commit

---
+++ src/Pages/Home/Cards2/Cards.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './Cards.css'
+
+const Cards = () => {
+    return (
+       <>
+        <div className='cards'>
+            <div class="container">
+            
+         <div class="image">
+            <img src="https://scontent.fdac99-1.fna.fbcdn.net/v/t39.30808-6/281760173_1067836044113090_7794491380868506137_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHdbe92oqWBT7NoN4DvyEv7oEQqCL0-txegRCoIvT63Fz1jQCbzEPrOpxgkF1xtxxBDL1tqugamnggg_NwGeCyY&_nc_ohc=uanvQo_N-QkAX_K6EuI&_nc_ht=scontent.fdac99-1.fna&oh=00_AT9-7l1RTa7gUlroZWU0P7k-C4d8bYLbdwcCh72E86ZDgw&oe=628D3AC0" alt="" />
+         </div>
+         <div class="card-content">
+            <div class="wrapper">
+               <div class="title">
+                  Adidas Originals
+               </div>
+               <p>
+                  Men's running tshirt
+               </p>
+               <span class="price">$29.99</span>
+               <div class="btns">
+                  <button>Buy now</button>
+                  <button>Add to cart</button>
+               </div>
+            </div>
+         </div>
+      </div>
+            <div class="container">
+         <div class="image">
+            <img src="https://scontent.fdac99-1.fna.fbcdn.net/v/t1.6435-9/123652913_717415279155170_6968995783453785650_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGQ7qh-I28ac6BpANMCjOeGOSbBMb19J6g5JsExvX0nqCnzqu25FraJ-hchAKPPdlX4MS23q8v5jKGFAzx3qxZa&_nc_ohc=JSYdibUZ3mUAX9HytKs&_nc_ht=scontent.fdac99-1.fna&oh=00_AT_lkwo3DWJLx_MEShd7xIki3ab9w7cwu_aLGmGF_G86Mw&oe=62AEECBD" alt="" />
+         </div>
+         <div class="card-content">
+            <div class="wrapper">
+               <div class="title">
+                  Adidas Originals
+               </div>
+               <p>
+                  Men's running tshirt
+               </p>
+               <span class="price">$29.99</span>
+               <div class="btns">
+                  <button>Buy now</button>
+                  <button>Add to cart</button>
+               </div>
+            </div>
+         </div>
+      </div>
+            <div class="container">
+         <div class="image">
+            <img src="https://scontent.fdac99-1.fna.fbcdn.net/v/t39.30808-6/277227732_1034169300813098_1160739421832369038_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=174925&_nc_eui2=AeHYMrGMkhu7ZMrNiU2fDhaFtWce__YJziS1Zx7_9gnOJO3rq3iHWmF1LtorNf7nmIUuqklWJijhf0ng7sOaA6Mo&_nc_ohc=Xvm6tEBTf-sAX-RkYxy&tn=39rt_vJeKiEZv_Cp&_nc_ht=scontent.fdac99-1.fna&oh=00_AT-RR5-FFY3KP8sQtjxJfz3noygwj5xpzPEzWKb7Ze4X-Q&oe=628E099D" alt="" />
+         </div>
+         <div class="card-content">
+            <div class="wrapper">
+               <div class="title">
+                  Adidas Originals
+               </div>
+               <p>
+                  Men's running tshirt
+               </p>
+               <span class="price">$29.99</span>
+               <div class="btns">
+                  <button>Buy now</button>
+                  <button>Add to cart</button>
+               </div>
+            </div>
+         </div>
+      </div>
+        </div>
+       </>
+    );
+};
+
+export default Cards;
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Cards from '../Cards2/Cards';
+
+import './Home.css'
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1 className='gradient-text'>This is Heading</h1>
+            <Cards></Cards>
+        </div>
+    );
+};
+
+export default Home;
